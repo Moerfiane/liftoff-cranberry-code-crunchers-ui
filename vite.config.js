44ab2c2bf81ee9e869ddf5f3f68ab163ad9a089a
noah.brandwein1 <noah.brandwein98@gmail.com>
2024-01-04 19:33:26 -0600

    Trying to view RestaurantList.js in ui

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Remove the esbuild configuration or reactJsx option
+
 });
