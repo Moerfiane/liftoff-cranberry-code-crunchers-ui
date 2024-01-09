cd7e9159038c7eadab215343d44785867b54c892
noah.brandwein1 <noah.brandwein98@gmail.com>
2024-01-08 18:54:45 -0600

    Trying to view RestaurantList.js in ui (switched back to jsx)

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
+//import { defineConfig } from 'vite';
+//import react from '@vitejs/plugin-react';
+//
+//// https://vitejs.dev/config/
+//export default defineConfig({
+//  plugins: [react()],
+//  esbuild: {
+//    jsxFactory: 'React.createElement',
+//    jsxFragment: 'React.Fragment',
+//    reactJsx: 'react',
+//  },
+//});
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // SS 1.6.24 set to the root to remove error
   esbuild: {
     jsxFactory: 'React.createElement',
     jsxFragment: 'React.Fragment',
-    reactJsx: 'react',
   },
 });
-
